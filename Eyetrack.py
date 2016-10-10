2e279613a4fc1bc95031c4017339794f4192a5b5
Mikerr1111 <miroyal@csumb.edu>
2016-10-10 11:30:06 -0700

    Add files via upload

--- Eyetrack.py
+++ Eyetrack.py
@@ -3,6 +3,9 @@ import cv2
 import time
 
 cap = cv2.VideoCapture(0)       #640,480
+s, im = cap.read() # captures image
+cv2.imshow("Test Picture", im) # displays captured image
+cv2.imwrite("test.jpg",im) # writes image test.jpg to disk
 w = 640
 h = 480
 
@@ -11,7 +14,7 @@ while(cap.isOpened()):
     if ret==True:
 
         #detect face
-        faces = cv2.CascadeClassifier('haarcascade_eye.xml')
+        faces = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
         detected = faces.detectMultiScale(frame, 1.3, 5)
 
         #draw square
@@ -19,7 +22,7 @@ while(cap.isOpened()):
                 cv2.rectangle(frame, (x,y), ((x+w),(y+h)), (0,0,255),1)
                 cv2.line(frame, (x,y), ((x+w,y+h)), (0,0,255),1)
                 cv2.line(frame, (x+w,y), ((x,y+h)), (0,0,255),1)
-                #cv2.rectangle(frame, (x,y),((x+40),(y+40)),(0,0,255),1)        
+              # cv2.rectangle(frame, (x,y),((x+40),(y+40)),(0,0,255),1)        
 
 
         #show picture
